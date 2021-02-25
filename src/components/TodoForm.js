c78983ba4e753a409a37840fe17b27e4d4e52cf2
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-25 14:41:46 -0800

    Add handleSubmit in form tag

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -8,7 +8,7 @@ function TodoForm() {
   }
 
   return (
-      <form className="todo-form">
+      <form className="todo-form" onSubmit={handleSubmit}>
         <input type="text" placeholder="Add a todo" value={input} name="text" className="todo-input"/>
         <button className="todo-button">Add todo</button>
       </form>
