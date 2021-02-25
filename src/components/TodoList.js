b79f84129879b9f6c224dc6cf7c357a56a0734d7
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-25 15:38:46 -0800

    Add addTodo function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,6 +3,18 @@ import TodoForm from './TodoForm';
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
+
+  const addTodo = todo => {
+    if(!todo.text || /^\s*$/.test(todo.text)) {
+      return
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos)
+    console.log(...todos);
+  }
+
   return (
     <div>
       <h1>Plan for Today?</h1>
