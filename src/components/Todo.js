4f81bd918889fb53287ab786af520ae76ebb2a3a
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-25 17:12:59 -0800

    Add onClick for delete/edit icon

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,7 @@ import { RiCloseCircleLine } from 'react-icons/ri'
 import { TiEdit } from 'react-icons/ti'
 
 
-function Todo({todos, completeTodo}) {
+function Todo({todos, completeTodo, removeTodo}) {
   const [edit, setEdit] = useState({
     id: null,
     value: ''
@@ -16,8 +16,8 @@ function Todo({todos, completeTodo}) {
         {todo.text}
       </div>
       <div className="icons">
-        <RiCloseCircleLine />
-        <TiEdit />
+        <RiCloseCircleLine onClick={() => removeTodo(todo.id)} className="delete-icon"/>
+        <TiEdit onClick={() => setEdit({id: todo.id, value: todo.text})} className="edit-icon"/>
       </div>
     </div>
   ))
